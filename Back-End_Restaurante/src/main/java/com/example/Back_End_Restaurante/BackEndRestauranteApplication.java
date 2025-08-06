a8f8ec69c9d83f8be91830235019c907c546c7b0
Luc6sx <mattoscanela65@gmail.com>
2025-08-06 14:52:08 -0300

    feat: inicia projeto com as pastas back e front já construida * Back-End_Restaurante: Java, SpringBoot, Hebernate * Front-End_Restaurante: Angular

---
+++ Back-End_Restaurante/src/main/java/com/example/Back_End_Restaurante/BackEndRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Back_End_Restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndRestauranteApplication.class, args);
+	}
+
+}
