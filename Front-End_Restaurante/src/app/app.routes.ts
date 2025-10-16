8d38fb587608e8a0f46ce60cc47b4a1ab0e2c0a7
Robert Silva <robert.silva@uvvnet.com.br>
2025-10-16 08:50:19 -0300

    feat: Adição de componentes Home, Header e Footer; Estilização dos componentes em relação ao protótipo

--- Front-End_Restaurante/src/app/app.routes.ts
+++ Front-End_Restaurante/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { LoginCliente } from './pages/login-cliente/login-cliente';
 import { SeletorLogin } from './pages/seletor-login/seletor-login';
 import { Cardapio } from './pages/cardapio/cardapio';
 import { Pedidos } from './pages/pedidos/pedidos';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    { path: "", component: SeletorLogin },
+    { path: "", component: Home },
+    { path: "seletor-login", component: SeletorLogin },
     { path: "login-funcionario", component: LoginFuncionario },
     { path: "login-cliente", component: LoginCliente },
     { path: "cardapio", component: Cardapio },
---
+++ Front-End_Restaurante/src/app/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+  // protected imagens = [
+  //   'assets/imgs/home1.jpg',
+  //   'assets/imgs/home2.jpg',
+  //   'assets/imgs/home3.jpg'
+  // ];
+}
