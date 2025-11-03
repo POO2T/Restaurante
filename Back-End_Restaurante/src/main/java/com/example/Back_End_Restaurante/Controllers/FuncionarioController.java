b3986e145797f96c7c05d714575b973ab46c3328
Robert <robert.silva@uvvnet.com.br>
2025-11-03 19:01:12 -0300

    fix: Ajustando login de funcionario

--- Back-End_Restaurante/src/main/java/com/example/Back_End_Restaurante/Controllers/FuncionarioController.java
+++ Back-End_Restaurante/src/main/java/com/example/Back_End_Restaurante/Controllers/FuncionarioController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/funcionarios")
+@RequestMapping("/api/funcionarios")
 @CrossOrigin(origins = "*")
 public class FuncionarioController {
 
