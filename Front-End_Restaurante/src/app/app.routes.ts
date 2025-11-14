35051dfaf6db3f746072280bb253231c8c441b90
Robert Silva <robert.silva@uvvnet.com.br>
2025-11-14 08:47:59 -0300

    feat: Adicionando rota para Produtos

--- Front-End_Restaurante/src/app/app.routes.ts
+++ Front-End_Restaurante/src/app/app.routes.ts
@@ -15,6 +15,7 @@ import { Pedidos } from './pages/funcionarios/pedidos/pedidos';
 import { Usuarios } from './pages/funcionarios/usuarios/usuarios';
 
 import { AuthGuard } from './guards/auth.guard';
+import { Produtos } from './pages/funcionarios/produtos/produtos';
 
 export const routes: Routes = [
     { path: "", component: Home },
@@ -27,6 +28,7 @@ export const routes: Routes = [
     { path: "funcionario/pedidos", component: Pedidos, canActivate: [AuthGuard], data: { role: 'FUNCIONARIO' } },
     { path: "funcionario/mesas", component: Mesas, canActivate: [AuthGuard], data: { role: 'FUNCIONARIO' } },
     { path: "funcionario/usuarios", component: Usuarios, canActivate: [AuthGuard], data: { role: 'FUNCIONARIO' } },
+    { path: "funcionario/produtos", component: Produtos, canActivate: [AuthGuard], data: { role: 'FUNCIONARIO' } },
 
     { path: "**", redirectTo: "", pathMatch: "full" } // Rota coringa para páginas não encontradas
 ];
