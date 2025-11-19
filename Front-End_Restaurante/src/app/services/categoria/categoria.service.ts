ee14ff751f9044c0ff9c27bbbda7a760189bb4de
Robert Silva <robertvaladao007@gmail.com>
2025-11-19 16:20:40 -0300

    feat: Criação de Produto e Categoria Service

--- Front-End_Restaurante/src/app/services/categoria/categoria.service.ts
+++ Front-End_Restaurante/src/app/services/categoria/categoria.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+
+import { ApiService } from '../api';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { Categoria } from '../../models/categoria.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
   
+  private readonly endpoint = '/categorias';
+  private apiService = inject(ApiService);
+
+  constructor() { }
+
+  getCategorias(): Observable<Categoria[]> {
+    return this.apiService.get<Categoria[]>(this.endpoint).pipe(
+      tap((categorias) => {
+        console.log('Categorias:', categorias);
+      }),
+      catchError((error) => {
+        console.error('Error fetching categorias:', error);
+        return throwError(() => new Error('Failed to fetch categorias'));
+      })
+    );
+  }
+
+  postCategoria(categoria: Partial<Categoria>): Observable<Categoria> {
+    return this.apiService.post<Categoria>(this.endpoint, categoria).pipe(
+      tap((newCategoria) => {
+        console.log('Categoria criada:', newCategoria);
+      }),
+      catchError((error) => {
+        console.error('Error creating categoria:', error);
+        return throwError(() => new Error('Failed to create categoria'));
+      })
+    );
+  }
+
 }
